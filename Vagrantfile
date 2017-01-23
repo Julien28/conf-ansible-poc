a76aa1468becf87fe551397db13aa2906bc64df2
DOUCET <julien.doucet.externe@capgemini.com>
2017-01-23 17:35:14 +0100

    20170123_1734: Stabilisation du socle POC.

--- Vagrantfile
+++ Vagrantfile
@@ -3,28 +3,37 @@
 
 VAGRANTFILE_API_VERSION = "2"
 
+# liste d'instance Ansible
 ansible = [
   { :hostname => 'poc-ansible',   :ip => '192.168.0.100', :box => 'ubuntu/xenial64' },
 ]
 
+# liste d'instances POC
 nodes = [
   { :hostname => 'poc-web1',      :ip => '192.168.0.101', :box => 'ubuntu/xenial64' },
   { :hostname => 'poc-web2',    :ip => '192.168.0.102', :box => 'ubuntu/xenial64' },
   { :hostname => 'poc-bdd1',    :ip => '192.168.0.103', :box => 'ubuntu/xenial64' },
 ]
 
-Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+# choix de version de l'API Vagrant
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|			
 
+  # pour chaque éléments dans la liste ansible
   ansible.each do |ans|
+    # création de l'enveloppe de la VM
     config.vm.define ans[:hostname] do |ansconfig|
+	  # paramétrage de la VM courante (de la liste configurée)
       ansconfig.vm.box = "ubuntu/xenial64"
       ansconfig.vm.hostname = ans[:hostname] + ".local"
       ansconfig.vm.network :private_network, ip: ans[:ip]
+	  # copie de fichiers du répertoire courant vers les destinations configurées
       ansconfig.vm.provision "file", source: "id_rsa", destination: "/tmp/id_rsa"
 	  ansconfig.vm.provision "file", source: "id_rsa.pub", destination: "/tmp/id_rsa.pub"
 	  ansconfig.vm.provision "file", source: "hosts", destination: "/tmp/hosts"
+	  # envoie du script dans la VM et l'execute
       ansconfig.vm.provision "shell", path: "ansible.bash"
-      memory = ans[:ram] ? ans[:ram] : 256;
+      memory = ans[:ram] ? ans[:ram] : 512;
+	  # customisation de la VM
       ansconfig.vm.provider :virtualbox do |vb|
         vb.customize [
           "modifyvm", :id,
@@ -34,14 +43,19 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 	  end
     end
   end
-  
+ 
+  # pour chaque éléments dans la liste nodes 
   nodes.each do |node|
+    # création de l'enveloppe des VM
     config.vm.define node[:hostname] do |nodeconfig|
+	  # paramétrage de la VM courante (de la liste configurée)
       nodeconfig.vm.box = "ubuntu/xenial64"
       nodeconfig.vm.hostname = node[:hostname] + ".local"
       nodeconfig.vm.network :private_network, ip: node[:ip]
+	  # envoie du script dans la VM et l'execute
       nodeconfig.vm.provision "shell", path: "authorized_keys.bash"
-      memory = node[:ram] ? node[:ram] : 256;
+      memory = node[:ram] ? node[:ram] : 512;
+	  # customisation de la VM
       nodeconfig.vm.provider :virtualbox do |vb|
         vb.customize [
           "modifyvm", :id,
--- ansible.bash
+++ ansible.bash
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+# manager les repositories
 sudo apt-get -y install software-properties-common
+# installation repository officiel pour ubuntu
 sudo apt-add-repository -y ppa:ansible/ansible
+# mise à jour du referentiel des packages suite à l'installation du repository d'Ansible
 sudo apt-get -y update
 sudo apt-get -y install ansible git
 sudo rm -f /etc/ansible/hosts
 sudo mv /tmp/hosts /etc/ansible/hosts
+# ajoute un urilisateur dans le groupe primaire
 sudo useradd -c "ansible admin" -g sudo -m -s /bin/bash ansible
 sudo echo "ansible:ansible" | chpasswd
+# créer le fichier .ssh
 sudo mkdir /home/ansible/.ssh
+# permet de modifier la permission d'accès (lire, écrire et executer le fichier)
 sudo chmod 700 /home/ansible/.ssh
 sudo mv /tmp/id_rsa /home/ansible/.ssh/id_rsa
+# permet de modifier la permission d'accès (lire et écrire le fichier)
 sudo chmod 600 /home/ansible/.ssh/id_rsa
 sudo mv /tmp/id_rsa.pub /home/ansible/.ssh/id_rsa.pub
 sudo chmod 600 /home/ansible/.ssh/id_rsa.pub
-sudo chown -R ansible:sudo /home/ansible/
-sudo su - ansible
+# ne vérifie pas strictement l'identité ssh du domaine github.com pour l'utilisateur ansible (retrait de l'interactivité)
 sudo echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /home/ansible/.ssh/config
-sudo cd /home/ansible/
+# change de propriétaire
+sudo chown -R ansible:sudo /home/ansible/
+# Change user and Move to ansible home.
+sudo su - ansible
+# passage a ansible avec son environnement puis lance le clonage du repository distant
 sudo su - ansible -c "git clone git@github.com:Julien28/ansible-poc.git"
+# concatene la clé publique qui sera utilisée par ansible dans le fichier authorized_keys de l'utilisateur ubuntu ( avec >> pour ne pas écraser le fichier)
 sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDCgd1aFqw7Z4TXfmFG1Shs82ghIrSgyc5xY03AsAtahndr3PRKun+120FiDsLAAi7/xKnhCgKPrBakP4cIMJRu29eon8W5d8l4WxBRl1S9ElKp88zRDjE3uIYTuQh0phOynR8477pIGXA7IwKNJT2jsngDbagzi6XRbrkBzPf7N68fwyGdqkoi0vlbRn6PRuMLxGj1V0fySXDGJ17YALTV5JLF91HkE2xTW+DamGKZZG2mvTWuqRAfX/cXd6GNr6hGWXyVi8npW17xaCTxVCFHzpIbnXNo2m2spkBodr4AlgG45Pftrjkz8fsoNdqJaCcb+e2VZ/P1B7K+PRTwrpChhJ8W/HAU91Lq/Vyi0ngqQjjWa3NgeR/xbNMIEn9lH0c8AYjOmijKdnD9e2IpQd7phfLtYLBAWQ2/ruVIb1eT704Kscm5q85Ad86J7PYHvAso+jaITSz2HRH/quOstKQDqiup/SzujBTgy9AWYzRKgY1Fj4yLJ+UUSgssV80C9y0iwdbc/ptTQJbjQdofanB8NAvtXOsohA4cW3OImtkX6aMxAqKN0fYC9l3bB+BwG1HprCKh4QtiT1V9hhOaep6Sy65nap4CUCRxWA0gPgquJvFDQvYPYamKYCeytyVJIgma0bEwyyPJLw0lgv/pgpQfFGrQbe49UQRFBpkOfBz3yw== poc-ansible" >> /home/ubuntu/.ssh/authorized_keys
-sudo sed -i.bak_vagrant 's/#host_key_checking/host_key_checking/g' /etc/ansible/ansible.cfg
+# désactive la vérification stricte des serveurs sous contrôle d'Ansible + set l'utilisateur remote par défaut en ubuntu + demande l'élévation de privilège.
+sudo sed -i.bak_vagrant -e 's/#host_key_checking/host_key_checking/g' -e '/remote_user/s/.*/remote_user = ubuntu/g' -e 's/^#become/become/g' /etc/ansible/ansible.cfg
